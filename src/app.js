018677de073a59fce299b0640848c2d9af66bbdc
micaelbittencourt_dev <micaelbittencourt_dev@gmail.com>
2022-01-26 11:55:19 -0300

    instalado mongoose e criado o model de product

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://dev:cc3bfcyPsMPo4MXT@cluster0.imxz0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
 // Carrega as rotas
 const indexRoute = require('./routes/index-route');
