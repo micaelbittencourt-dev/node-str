5a805ecbdd88aaa9377d55884c214b12ee8d2461
micaelbittencourt_dev <micaelbittencourt_dev@gmail.com>
2022-01-26 11:36:15 -0300

    organizando os controllers

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -1,12 +1,8 @@
 'use strict'
 const express = require('express');
 const router = express.Router();
+const controller = require('./../controllers/index-controller');
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
+router.get('/', controller.get);
 
 module.exports = router;
